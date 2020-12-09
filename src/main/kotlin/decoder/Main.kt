0e76b0d27b49527403562f2b25f108cc3a643a54
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2020-12-09 23:48:00 +0300

    init

--- src/main/kotlin/decoder/Main.kt
+++ src/main/kotlin/decoder/Main.kt
@@ -4,11 +4,10 @@ import ux
 
 fun main() = ux("output file name") { data ->
     val d = monotone.decode(data)
-
     val i = d[0]
     val m = d.drop(1)
-
     val s = mtf.decode(m)
+    val result = barrows.decode(s, i)
 
-    barrows.decode(s, i)
+    result
 }
